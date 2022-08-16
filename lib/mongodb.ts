77056ef4a2eaa2af366761105f34d06253ac49be
Rohit Dhas <rohitdhas666@gmail.com>
2022-08-16 22:18:50 +0530

    project configuration

---
+++ lib/mongodb.ts
@@ -0,0 +1,44 @@
+import { Db, MongoClient } from "mongodb";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_DB = process.env.DB_NAME;
+
+if (!MONGODB_URI) {
+  throw new Error("Define the MONGODB_URI environmental variable");
+}
+
+if (!MONGODB_DB) {
+  throw new Error("Define the MONGODB_DB environmental variable");
+}
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
+export async function connectToDatabase() {
+  // check the cached.
+  if (cachedClient && cachedDb) {
+    // load from cache
+    return {
+      client: cachedClient,
+      db: cachedDb,
+    };
+  }
+
+  // const opts = {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // };
+
+  let client: any = new MongoClient(MONGODB_URI);
+  await client.connect();
+  let db = client.db(MONGODB_DB);
+
+  // set cache
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
+}
