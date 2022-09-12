f9cdfee0410e11f5134cf7f76bc11e0fe26a0728
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-12 20:56:19 +0530

    small update

--- utils/db.utils.ts
+++ utils/db.utils.ts
@@ -1,7 +1,7 @@
 import { connectToDatabase, ConnectionType } from '../lib/mongodb';
 
 export const getProducts = async () => {
-  const { db }: any = await connectToDatabase();
+  const { db }: ConnectionType = await connectToDatabase();
   const products = await db.collection('products').find({}).toArray();
   return products;
 }
