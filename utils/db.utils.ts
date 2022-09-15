a663ccb000261dc4691bbb047071430733cde011
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-15 20:32:33 +0530

    added all get apis

--- utils/db.utils.ts
+++ utils/db.utils.ts
@@ -1,9 +1,12 @@
 import { connectToDatabase, ConnectionType } from '../lib/mongodb';
+import { buildFilterQuery } from './main.utils';
 import { ObjectId } from 'mongodb';
 
-export const getProducts = async () => {
+export const getProducts = async (filters: any) => {
+  const query = buildFilterQuery(filters);
   const { db }: ConnectionType = await connectToDatabase();
-  const products = await db.collection('products').find({}).toArray();
+
+  const products = await db.collection('products').find(query).toArray();
   return products;
 }
 
@@ -11,4 +14,33 @@ export const getOneProduct = async (id: string) => {
   const { db }: ConnectionType = await connectToDatabase();
   const product = await db.collection('products').findOne({ _id: new ObjectId(id) });
   return product;
+}
+
+export const getOrders = async (userId: string) => {
+  const { db }: ConnectionType = await connectToDatabase();
+  const orders = await db.collection('orders').find({ user: new ObjectId(userId) }).toArray();
+  return orders;
+}
+
+export const searchAutocomplete = async (input: string) => {
+  const { db }: ConnectionType = await connectToDatabase();
+  const products = await db.collection('products').aggregate([
+    {
+      $search: {
+        index: "default",
+        compound: {
+          should: [
+            {
+              autocomplete: {
+                query: input,
+                path: "title",
+                fuzzy: { maxEdits: 2, prefixLength: 3 },
+              },
+            }
+          ],
+        },
+      },
+    }
+  ]).toArray();
+  return products;
 }
