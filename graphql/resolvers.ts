fddf144bb4f206cb75896636e430cce22516f604
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-15 20:32:05 +0530

    coded new resolvers

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,13 +1,23 @@
-import { getProducts, getOneProduct } from "../utils/db.utils"
+import { getProducts, getOneProduct, getOrders, searchAutocomplete } from "../utils/db.utils"
 
 export const resolvers = {
   Query: {
-    products: async () => {
-      return await getProducts()
+    products: async (_parent: any, args: any) => {
+      const { filters } = args;
+      return await getProducts(filters);
     },
     product: async (_parent: any, args: any) => {
       const { id } = args;
       return await getOneProduct(id);
+    },
+    orders: async (_parent: any, args: any) => {
+      const { userId } = args;
+      return await getOrders(userId);
+    },
+    autocomplete: async (_parent: any, args: any) => {
+      const { input } = args;
+      if (!input) return await getProducts({});
+      return await searchAutocomplete(input);
     }
   }
 }
