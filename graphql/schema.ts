068b65e885ab703d32cdfae8ea73c3a4fe7ac7e0
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-16 20:22:57 +0530

    added mutations to schema

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,7 +1,11 @@
 import { gql } from 'apollo-server-micro';
 
 const typeDefs = gql`
+  scalar JSON
+  scalar JSONObject
+
   type Address {
+    id: ID!
     appartment: String
     street: String
     city: String
@@ -55,6 +59,18 @@ const typeDefs = gql`
     createdAt: String
   }
 
+  type ResponseMessage {
+    message: String
+    isError: Boolean
+    data: JSON
+  }
+
+  enum UpdateType {
+    ADD
+    UPDATE
+    DELETE
+  }
+
   input Filters {
     _id: String
     colors: [String]
@@ -65,12 +81,40 @@ const typeDefs = gql`
     exclusive: Boolean
   }
 
+  input CartItemInput {
+    productId: String
+    quantity: Int
+  }
+
+  input OrderInput {
+    products: [CartItemInput!]
+    userId: String
+  }
+
+  input AddressUpdate {
+    id: ID!
+    appartment: String
+    street: String
+    city: String
+    state: String
+    country: String
+    zipCode: Int
+    phone: Int
+    updateType: UpdateType
+  }
+
   type Query {
     products(filters: Filters): [Product]
-    product(id: ID): Product
+    product(id: ID!): Product
     user(id: ID!): User!
     orders(userId: ID!): [Order]
-    autocomplete(input: String): [Product]
+    autocomplete(input: String!): [Product]
+  }
+
+  type Mutation {
+    createOrder(userId: ID!, order: OrderInput!): ResponseMessage
+    updateUsername(userId: ID!, update: String!): ResponseMessage
+    updateAddress(userId: ID!, update: AddressUpdate): ResponseMessage
   }
 `;
 
