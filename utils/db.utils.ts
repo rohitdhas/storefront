7b3eee2b5de7c349d8c42c68a6ada0a473693745
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-16 21:45:20 +0530

    added mutation utils

--- utils/db.utils.ts
+++ utils/db.utils.ts
@@ -2,6 +2,7 @@ import { connectToDatabase, ConnectionType } from '../lib/mongodb';
 import { buildFilterQuery } from './main.utils';
 import { ObjectId } from 'mongodb';
 
+// Query - GET
 export const getProducts = async (filters: any) => {
   const query = buildFilterQuery(filters);
   const { db }: ConnectionType = await connectToDatabase();
@@ -43,4 +44,41 @@ export const searchAutocomplete = async (input: string) => {
     }
   ]).toArray();
   return products;
+}
+
+
+// Mutations - POST, PUT, DELETE
+
+export const createOrder = async (order: any) => {
+  const { db }: ConnectionType = await connectToDatabase();
+  const { insertedId } = await db.collection('orders').insertOne(order);
+  return { message: "Order created successfully", isError: false, data: insertedId };
+}
+
+export const updateUsername = async (userId: string, update: string) => {
+  const { db }: ConnectionType = await connectToDatabase();
+  const res = await db.collection('users').updateOne({ _id: new ObjectId(userId) }, { $set: { username: update } });
+  return { message: "Username updated successfully", isError: false, data: res };
+}
+
+export const updateAddress = async (userId: string, update: any) => {
+  const { db }: ConnectionType = await connectToDatabase();
+  const { updateType, address } = update;
+  let res;
+
+  if (updateType === "ADD") {
+    res = await db.collection('users').updateOne({ _id: new ObjectId(userId) }, { $push: { addresses: address } });
+  } else if (updateType === "UPDATE") {
+    res = await db.collection('users').updateOne({ _id: new ObjectId(userId), "addresses.$.id": address.id }, { $set: { "addresses.$": address } });
+  } else {
+    res = await db.collection('users').updateOne({ _id: new ObjectId(userId) }, { $pull: { addresses: { id: address.id } } });
+  }
+  return {
+    message: `Address ${updateType === 'ADD'
+      ? "Added"
+      : updateType === "UPDATE"
+        ? "Updated"
+        : "Deleted"} successfully`,
+    isError: false, data: res
+  };
 }
