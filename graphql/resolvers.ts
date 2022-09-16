405bc5642b45f37d72165326467a527fc1a3f8be
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-16 20:23:13 +0530

    added mutations to resolvers

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,4 +1,5 @@
 import { getProducts, getOneProduct, getOrders, searchAutocomplete } from "../utils/db.utils"
+import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
 
 export const resolvers = {
   Query: {
@@ -19,5 +20,18 @@ export const resolvers = {
       if (!input) return await getProducts({});
       return await searchAutocomplete(input);
     }
-  }
+  },
+  Mutation: {
+    createOrder: async (_parent: any, args: any, context: any) => {
+      const { order } = args;
+      console.log(context);
+      return {
+        message: 'Order created successfully',
+        isError: false,
+        data: order
+      }
+    }
+  },
+  JSON: GraphQLJSON,
+  JSONObject: GraphQLJSONObject,
 }
