ec1cfabbeaa515f2cead8704fa5aed3cc3b475db
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-17 16:54:16 +0530

    mics update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Layout from "../components/layout";
 import "primeicons/primeicons.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 }
