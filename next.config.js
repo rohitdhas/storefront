e64660417710f7c0f442f835b7b062be091698b2
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-18 11:31:40 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
