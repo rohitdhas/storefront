487c2ac5b576eed23d45dbdf09896f5391eab54e
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-19 20:31:47 +0530

    redux setup done

---
+++ redux/userSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+interface userDetailsPayload {
+  name: string;
+  email: string;
+  image: string;
+}
+
+interface IinitialState {
+  name: string;
+  email: string;
+  image: string;
+  cart: Object[];
+  wishlist: Object[];
+}
+
+const initialState: IinitialState = {
+  name: "",
+  email: "",
+  image: "",
+  cart: [],
+  wishlist: []
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateDetails: (state, action: PayloadAction<userDetailsPayload>) => {
+      const { name, email, image } = action.payload;
+      state.name = name;
+      state.email = email;
+      state.image = image;
+    },
+    addToCart: (state, action: PayloadAction<Object>) => {
+      state.cart.push(action.payload);
+    }
+  }
+})
+
+export const { updateDetails, addToCart } = userSlice.actions;
+export default userSlice.reducer;
