22cf552447c8ecddfc040459bef5421f7629e9f8
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-22 22:45:08 +0530

    added product cards

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,8 +1,66 @@
-import React from "react";
-import Head from "next/head";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import ProductCard from "../components/productCard";
 import Filter from "../components/filter";
+import Head from "next/head";
+import React from "react";
 
-const Products: React.FC = () => {
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "http://localhost:3000/api/graphql/",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query GetProducts {
+        products(filters: {}) {
+          _id
+          title
+          description
+          images
+          currentPrice
+          originalPrice
+          rating
+          stock
+          category
+          exclusive
+          tags
+          specifications {
+            key
+            value
+          }
+          color
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+interface Product {
+  _id: string;
+  title: string;
+  description: string;
+  images: string[];
+  currentPrice: number;
+  originalPrice: number;
+  rating: number;
+  stock: number;
+  category: string;
+  exclusive: boolean;
+  tags: string[];
+  specifications: Object[];
+  color: string;
+}
+
+const Products: React.FC<{ data: { products: Product[] } }> = ({ data }) => {
+  const { products } = data;
+
   return (
     <div className="px-6 pt-4">
       <Head>
@@ -20,7 +78,14 @@ const Products: React.FC = () => {
             results!
           </p>
         </div>
-        <Filter />
+        <div className="flex my-6">
+          <Filter />
+          <div className="grid grid-cols-4 gap-4 flex-1 ml-6">
+            {products.map((product) => {
+              return <ProductCard key={product._id} product={product} />;
+            })}
+          </div>
+        </div>
       </main>
     </div>
   );
