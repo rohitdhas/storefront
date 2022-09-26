1f995cd5ae6f48beaa124adbc3cd8b03b410a206
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-26 22:15:00 +0530

    product page update

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,7 +1,17 @@
+import ProductDetailsView from "../components/productDetailsView";
+import {
+  addToWishlist,
+  addToCart,
+  notify,
+  removeFromWishlist,
+} from "../utils/main.utils";
+import React, { useEffect, useState, useRef } from "react";
 import ProductCard from "../components/productCard";
 import { productsQuery } from "../utils/gpl.util";
+import { updateWishlist, updateCart } from "../redux/userSlice";
+import { useDispatch } from "react-redux";
+import { Toast } from "primereact/toast";
 import Filter from "../components/filter";
-import React, { useEffect, useState } from "react";
 import Head from "next/head";
 
 interface Product {
@@ -22,6 +32,10 @@ interface Product {
 
 const Products: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product>();
+  const [sidebarVisible, setSidebarVisible] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const toast = useRef<any>();
 
   useEffect(() => {
     getProducts();
@@ -32,6 +46,37 @@ const Products: React.FC = () => {
     setProducts(data.products);
   }
 
+  const wishlistProduct = (product: Product) => {
+    const updatedList = addToWishlist(product);
+    dispatch(updateWishlist({ updatedProductList: updatedList }));
+    notify(
+      {
+        title: "Product Wishlisted",
+        message: `${product.title} - Added to your wishlist!`,
+        type: "info",
+      },
+      toast
+    );
+  };
+
+  const unWishlistProduct = (product: Product) => {
+    const updatedList = removeFromWishlist(product._id);
+    dispatch(updateWishlist({ updatedProductList: updatedList }));
+  };
+
+  const addProductToCart = (product: Product) => {
+    const updatedCart = addToCart(product);
+    dispatch(updateCart({ updatedCart }));
+    notify(
+      {
+        title: "Added to Cart",
+        message: `${product.title} - Added to your cart!`,
+        type: "success",
+      },
+      toast
+    );
+  };
+
   return (
     <div className="px-6 pt-4">
       <Head>
@@ -41,7 +86,15 @@ const Products: React.FC = () => {
           content="Explore the greatest product deals on the internet"
         />
       </Head>
+      <Toast ref={toast} />
       <main>
+        <ProductDetailsView
+          product={selectedProduct}
+          isVisible={sidebarVisible}
+          wishlistProduct={wishlistProduct}
+          addProductToCart={addProductToCart}
+          toggle={() => setSidebarVisible(false)}
+        />
         <div>
           <h3 className="text-2xl font-bold">Products</h3>
           <p className="text-sm text-info">
@@ -53,7 +106,19 @@ const Products: React.FC = () => {
           <Filter />
           <div className="grid grid-cols-4 gap-4 flex-1 ml-6">
             {products.map((product) => {
-              return <ProductCard key={product._id} product={product} />;
+              return (
+                <ProductCard
+                  quickViewToggle={() => setSidebarVisible(true)}
+                  wishlistProduct={wishlistProduct}
+                  unWishlistProduct={unWishlistProduct}
+                  addProductToCart={addProductToCart}
+                  setSelectedProduct={(product: Product) =>
+                    setSelectedProduct(product)
+                  }
+                  key={product._id}
+                  product={product}
+                />
+              );
             })}
           </div>
         </div>
