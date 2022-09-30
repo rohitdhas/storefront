65c7b37e7151e6ea521c4fd123000bb9a06b472e
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-30 15:36:27 +0530

    added some top margin

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Navbar />
-      <main className="mt-24">{children}</main>
+      <main className="mt-28">{children}</main>
     </>
   );
 }
