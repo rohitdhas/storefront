071639bc3a804a31a5a23be605caf7d3f53113c0
Rohit Dhas <rohitdhas666@gmail.com>
2022-09-30 15:36:51 +0530

    built loader component ⌛

---
+++ components/loader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { ProgressSpinner } from "primereact/progressspinner";
+
+const Loader: React.FC<{ loading: boolean }> = ({ loading }) => {
+  if (!loading) return <></>;
+  return (
+    <motion.div
+      initial={{ y: 200 }}
+      animate={{ y: 0 }}
+      exit={{ y: 200 }}
+      className="flex justify-evenly align items-center
+     bg-slate-300 p-6 rounded-md fixed right-6 bottom-6 shadow-sm"
+    >
+      <ProgressSpinner
+        style={{ width: "40px", height: "40px" }}
+        strokeWidth="4"
+        animationDuration=".5s"
+      />
+      <p className="ml-3 font-bold text-slate-800">Loading.. Hold tight</p>
+    </motion.div>
+  );
+};
+
+export default Loader;
