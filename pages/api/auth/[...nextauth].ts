aa99b9902f110ebeddbd35db2cafcb7f83609589
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-05 18:19:21 +0530

    auth file changes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,15 +11,19 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
+      // authorization: {
+      //   params: {
+      //     prompt: "consent",
+      //     access_type: "offline",
+      //     response_type: "code"
+      //   }
+      // }
     })
   ],
+  pages: {
+    signIn: "/",
+    error: '/',
+  },
   callbacks: <any>{
     async signIn({ account, profile }: any) {
       if (account.provider === 'google') {
