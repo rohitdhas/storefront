169258d7675f27a799b7383e48c1d6d48cd8e98d
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-09 20:12:44 +0530

    stripe util works

--- utils/stripe.util.ts
+++ utils/stripe.util.ts
@@ -1,17 +1,11 @@
-import stripeClient from 'stripe';
-
-const stripe = new stripeClient(process.env.STRIPE_SECRET_KEY!,
-  {
-    stripeAccount: process.env.STRIPE_PUBLISHABLE_KEY!,
-    apiVersion: "2022-08-01"
-  });
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 async function createStripeOrder(lineItems: any) {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: lineItems,
     mode: 'payment',
-    success_url: `${process.env.CLIENT_URL}/orders`,
+    success_url: `${process.env.CLIENT_URL}/orders?success=true`,
     cancel_url: `${process.env.CLIENT_URL}/cart`,
   });
 
