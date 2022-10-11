ec3dfb635d37fbad9a79de1ae8d7dee8c68dd680
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-11 21:53:40 +0530

    added order status constants

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const ORDER_STATUS: any = {
+  Created: {
+    type: "warning",
+    icon: "pi-exclamation-triangle",
+  },
+  Received: {
+    type: "info",
+    icon: "pi-info-circle",
+  },
+  Delivered: {
+    type: "success",
+    icon: "pi-check",
+  },
+  Cancelled: {
+    type: "error",
+    icon: "pi-times",
+  },
+};
