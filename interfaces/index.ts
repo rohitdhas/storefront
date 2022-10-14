4f01069048a087e008290e4562ae1dfa4131317f
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-14 21:27:39 +0530

    created seprate file for interfaces

---
+++ interfaces/index.ts
@@ -0,0 +1,38 @@
+export interface IProduct {
+  _id: string;
+  title: string;
+  description: string;
+  images: string[];
+  currentPrice: number;
+  originalPrice: number;
+  rating: number;
+  stock: number;
+  category: string;
+  exclusive: boolean;
+  tags: string[];
+  specifications: Object[];
+  color: string;
+  quantity?: number;
+}
+
+export interface IFilters {
+  id: string;
+  colors: string[];
+  brands: string[];
+  categories: string[];
+  rating: string;
+  inStock: string;
+  priceRange: string[];
+  exclusive: string;
+}
+
+export interface IAddress {
+  id: string;
+  apartment: string;
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  zipCode: string;
+  phone: string;
+}
