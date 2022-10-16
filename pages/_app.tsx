1af9098ebb812a294a20189c3ca5ed216e16f92e
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-16 17:07:39 +0530

    added page loading state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,9 @@ import "primeicons/primeicons.css";
 import { Session } from "next-auth";
 import "../styles/globals.css";
 import "../styles/misc.scss";
+import React from "react";
+import Router from "next/router";
+import { PageLoader } from "../components/spinner";
 
 function MyApp({
   Component,
@@ -17,13 +20,37 @@ function MyApp({
 }: AppProps<{
   session: Session;
 }>) {
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <AnimatePresence mode="wait">
         <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          {loading ? (
+            <PageLoader />
+          ) : (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          )}
         </Provider>
       </AnimatePresence>
     </SessionProvider>
