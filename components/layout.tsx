480419700fef14087942fef99d27971eb20dfbe1
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-16 17:08:54 +0530

    layout update

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./navbar";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+
+  if (router.pathname === "/admin") {
+    return <main>{children}</main>;
+  }
+
   return (
     <>
       <Navbar />
