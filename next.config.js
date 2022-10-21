e9ab22eaab4a23fc278b461c4cfb3295d4880e7d
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-21 23:02:31 +0530

    added new packages

--- next.config.js
+++ next.config.js
@@ -9,12 +9,14 @@ const nextConfig = {
       "content.jdmagicbox.com",
       "pngimg.com",
       "rukminim1.flixcart.com",
-      "m.media-amazon.com"
+      "m.media-amazon.com",
+      "storefront-products.s3.amazonaws.com",
     ],
   },
   env: {
     CLIENT_URL: process.env.CLIENT_URL,
-  }
+    BUCKET_URL: process.env.BUCKET_URL,
+  },
 };
 
 module.exports = nextConfig;
